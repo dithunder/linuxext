43e8d03ba76d14a7ddadd166d2decbc965dcf543
service <service@deb03.localhost>
2023-05-15 17:14:54 +0300

    day01

---
+++ day01/packer/scripts/stage-1-kernel-update.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Установка репозитория elrepo
+sudo yum install -y https://www.elrepo.org/elrepo-release-8.el8.elrepo.noarch.rpm 
+# Установка нового ядра из репозитория elrepo-kernel
+yum --enablerepo elrepo-kernel install kernel-ml -y
+
+# Обновление параметров GRUB
+grub2-mkconfig -o /boot/grub2/grub.cfg
+grub2-set-default 0
+echo "Grub update done."
+# Перезагрузка ВМ
+shutdown -r now
