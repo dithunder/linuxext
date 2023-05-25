a2a1270328f9b95be229bd3591894a28114aca72
dithunder <dithunder@users.noreply.github.com>
2023-05-25 23:10:43 +0300

    Update create-raid.sh

--- day02/scripts/create-raid.sh
+++ day02/scripts/create-raid.sh
@@ -4,8 +4,8 @@ mkdir -p ~root/.ssh
 cp ~vagrant/.ssh/auth* ~root/.ssh
 yum install -y mdadm smartmontools hdparm gdisk
 mdadm --create --verbose /dev/md0 --level=5 --raid-devices=5 /dev/sdb /dev/sdc /dev/sdd /dev/sde /dev/sdf
-#echo "DEVICE partitions" >> /etc/mdadm/mdadm.conf
-#mdadm --detail --scan --verbose | awk '/ARRAY/{print}' >> /etc/mdadm/mdadm.conf
+echo "DEVICE partitions" >> /etc/mdadm.conf
+mdadm --detail --scan --verbose | awk '/ARRAY/{print}' >> /etc/mdadm.conf
 parted -s /dev/md0 mklabel gpt
 parted /dev/md0 mkpart primary ext4 0% 20% 
 parted /dev/md0 mkpart primary ext4 20% 40% 
@@ -15,5 +15,4 @@ parted /dev/md0 mkpart primary ext4 80% 100%
 for i in $(seq 1 5); do mkfs.ext4 /dev/md0p$i; done
 mkdir -p /raid/part{1,2,3,4,5}
 for i in $(seq 1 5); do mount /dev/md0p$i /raid/part$i; done
-mdadm --detail --scan | tee -a /etc/mdadm.conf
 cp /var/log/*.* /raid/part3
