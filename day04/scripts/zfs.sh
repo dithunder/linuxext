c9d5a9cbe685540ab8f5f624d0576f2a284b676c
service <dimkan@list.ru>
2023-06-16 12:04:08 +0300

    Добавление ДЗ 4

---
+++ day04/scripts/zfs.sh
@@ -0,0 +1,18 @@
+sudo -i 
+#yum update -y
+mkdir -p ~root/.ssh
+cp ~vagrant/.ssh/auth* ~root/.ssh
+yum install -y rsync
+yum install -y http://download.zfsonlinux.org/epel/zfs-release.el7_8.noarch.rpm
+#import gpg key 
+rpm --import /etc/pki/rpm-gpg/RPM-GPG-KEY-zfsonlinux
+#install DKMS style packages for correct work ZFS
+yum install -y epel-release kernel-devel zfs
+#change ZFS repo
+yum-config-manager --disable zfs
+yum-config-manager --enable zfs-kmod
+yum install -y zfs
+#Add kernel module zfs
+modprobe zfs
+#install wget
+yum install -y wget
